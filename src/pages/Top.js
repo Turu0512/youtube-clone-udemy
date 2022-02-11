0a4afc427ae43b2cfbf192bc6983d8ebcd464875
Turu0512 <hinataptyan@gmail.com>
2022-02-11 17:51:39 +0900

    API実装

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import Layout from "../components/Layout/Layout";
+import { fetchPopularData } from "../api/index";
+import { Store } from "../store/index";
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store);
+  useEffect(() => {
+    fetchPopularData().then((result) => {
+      console.log("data", result);
+      setGlobalState({
+        type: "SET_POPULAR",
+        payload: { popular: result.data.items },
+      });
+    });
+  }, []);
   return <Layout>Top Page</Layout>;
 };
 

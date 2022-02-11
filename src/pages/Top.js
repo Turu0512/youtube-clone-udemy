b6a611a20716cfad8bb567d2189114c9a2b5d8c7
Turu0512 <hinataptyan@gmail.com>
2022-02-11 18:40:52 +0900

    リスト表示の実装

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,6 +2,8 @@ import React, { useEffect, useContext } from "react";
 import Layout from "../components/Layout/Layout";
 import { fetchPopularData } from "../api/index";
 import { Store } from "../store/index";
+import VideoGridItem from "../components/Layout/VideoGridItem/VideoGridItem";
+import VideoGrid from "../components/Layout/VideoGrid/VideoGrid";
 
 const Top = () => {
   const { globalState, setGlobalState } = useContext(Store);
@@ -14,7 +16,23 @@ const Top = () => {
       });
     });
   }, []);
-  return <Layout>Top Page</Layout>;
+  return (
+    <Layout>
+      <VideoGrid>
+        {globalState.popular &&
+          globalState.popular.map((popular) => {
+            return (
+              <VideoGridItem
+                id={popular.id}
+                key={popular.id}
+                src={popular.snippet.thumbnails.high.url}
+                title={popular.snippet.title}
+              />
+            );
+          })}
+      </VideoGrid>
+    </Layout>
+  );
 };
 
 export default Top;
